409c768e4d2363decbb6df4925c754079dcd2731
Subhamtyagi684 <subhamtyagi85@gmail.com>
2022-03-07 18:55:03 +0530

    replaced hard code background title to payload.notification.title

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -11,90 +11,93 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-if (firebase.messaging.isSupported()){
-    const messaging = firebase.messaging();
-    navigator.serviceWorker.register('/js/sw.js').then((registration) => {
-        messaging.useServiceWorker(registration);
-        messaging.requestPermission().then(function() {
-            console.log('Notification Permission Granted!');
-            messaging.getToken({ vapidKey: "BEwZLZAXyfUGCQmEfS8To-es8P65QRn2UKvBE7koxtpWTDYeKXuEgDLId-WWuCyaGlyCY2ey4wkJ5RxVrvJ-lgs" }).then((ntoken) => {
-                if (ntoken) {
-                    console.log(ntoken);
-                    sendTokenToServer(ntoken);
-                } else {
-                    console.log('No registration token available. Request permission to generate one.');
+if(firebase.messaging)
+{
+    if (firebase.messaging.isSupported()){
+        const messaging = firebase.messaging();
+        navigator.serviceWorker.register('/js/sw.js').then((registration) => {
+            messaging.useServiceWorker(registration);
+            messaging.requestPermission().then(function() {
+                console.log('Notification Permission Granted!');
+                messaging.getToken({ vapidKey: "BEwZLZAXyfUGCQmEfS8To-es8P65QRn2UKvBE7koxtpWTDYeKXuEgDLId-WWuCyaGlyCY2ey4wkJ5RxVrvJ-lgs" }).then((ntoken) => {
+                    if (ntoken) {
+                        console.log(ntoken);
+                        sendTokenToServer(ntoken);
+                    } else {
+                        console.log('No registration token available. Request permission to generate one.');
+                        setTokenSentToServer(false);
+                    }
+                }).catch((err) => {
+                    console.log('An error occurred while retrieving token. ', err);
                     setTokenSentToServer(false);
-                }
-            }).catch((err) => {
-                console.log('An error occurred while retrieving token. ', err);
-                setTokenSentToServer(false);
+                });
+            }).catch(function(err) {
+                console.log('Notification Permission Denied!');
             });
-        }).catch(function(err) {
-            console.log('Notification Permission Denied!');
+        }).catch(function(error) {
+            // registration failed
+            console.log('Registration failed with ' + error);
         });
-    }).catch(function(error) {
-        // registration failed
-        console.log('Registration failed with ' + error);
+
+
+    function sendTokenToServer(ntoken) {
+        if (!isTokenSentToServer()) {
+            console.log('Sending token to server...');
+            // var data = {};
+            // var url = "";
+            // $.ajax({
+            //     url: url,
+            //     type: "POST",
+            //     data: JSON.stringify(data),
+            //     dataType: "text",
+            //     processData: false,
+            //     contentType: "application/json; charset=utf-8",
+            //     success: function (data, status, jqXHR) {
+            //         console.log("successfully retrieved token");
+            //     },
+            //     error: function (err) {
+            //         console.log(err);
+            //     },
+            //     complete: function (jqXHR, status) {
+            //         console.log("request complete");
+            //     }
+            // });
+            setTokenSentToServer(true);
+        } else {
+            console.log('Token already sent to server so won\'t send it again ');
+        }
+    }
+
+    function isTokenSentToServer() {
+        return window.localStorage.getItem('sentToServer') == 1;
+    }
+
+    function setTokenSentToServer(sent) {
+        window.localStorage.setItem('sentToServer', sent ? 1 : 0);
+    }
+
+
+    messaging.onMessage( function(payload) {
+        console.log('++++++++here+++++++++',payload)
+        var title = payload.notification.title?payload.notification.title:'Title';
+        var options = {
+            body: payload.notification.body?payload.notification.body:'Description',
+            icon: payload.notification.icon,
+            image: payload.notification.image,
+            data: {
+                click_action: payload.notification.web_url?payload.notification.web_url:"https://www.google.com"
+            }
+        };
+        var notification = new Notification(title, options);
+        notification.onclick = function(event) {
+            event.preventDefault(); // prevent the browser from focusing the Notification's tab
+            window.open(event.currentTarget.data.click_action, '_blank');
+            notification.close();
+        };
     });
 
-
-function sendTokenToServer(ntoken) {
-    if (!isTokenSentToServer()) {
-        console.log('Sending token to server...');
-        // var data = {};
-        // var url = "";
-        // $.ajax({
-        //     url: url,
-        //     type: "POST",
-        //     data: JSON.stringify(data),
-        //     dataType: "text",
-        //     processData: false,
-        //     contentType: "application/json; charset=utf-8",
-        //     success: function (data, status, jqXHR) {
-        //         console.log("successfully retrieved token");
-        //     },
-        //     error: function (err) {
-        //         console.log(err);
-        //     },
-        //     complete: function (jqXHR, status) {
-        //         console.log("request complete");
-        //     }
-        // });
-        setTokenSentToServer(true);
-    } else {
-        console.log('Token already sent to server so won\'t send it again ');
     }
-}
-
-function isTokenSentToServer() {
-    return window.localStorage.getItem('sentToServer') == 1;
-}
-
-function setTokenSentToServer(sent) {
-    window.localStorage.setItem('sentToServer', sent ? 1 : 0);
-}
-
-
-messaging.onMessage( function(payload) {
-    console.log('++++++++here+++++++++',payload)
-    var title = payload.notification.title?payload.notification.title:'Title';
-    var options = {
-        body: payload.notification.body?payload.notification.body:'Description',
-        icon: payload.notification.icon,
-        image: payload.notification.image,
-        data: {
-            click_action: payload.notification.web_url?payload.notification.web_url:"https://www.google.com"
-        }
-    };
-    var notification = new Notification(title, options);
-    notification.onclick = function(event) {
-        event.preventDefault(); // prevent the browser from focusing the Notification's tab
-        window.open(event.currentTarget.data.click_action, '_blank');
-        notification.close();
-    };
-});
-
-}
-else{
-    console.log('FCM is not supported')
+    else{
+        console.log('FCM is not supported')
+    }
 }
