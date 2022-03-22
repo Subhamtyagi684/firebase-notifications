a550be13cd21c474732057e291076159b5671327
Subhamtyagi684 <subhamtyagi85@gmail.com>
2022-03-22 15:23:22 +0530

    fixed service worker

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,14 +1,27 @@
+importScripts('https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js');
 
 
-    self.addEventListener('install', function(event) {
-      console.log('service worker is installing...')
-      
+  const firebaseConfig = {
+      apiKey: "AIzaSyBDQjRGoc1Eb0rZt5QLaZSaoTEtX2apL7k",
+      authDomain: "fir-project-affb7.firebaseapp.com",
+      projectId: "fir-project-affb7",
+      storageBucket: "fir-project-affb7.appspot.com",
+      messagingSenderId: "753676898557",
+      appId: "1:753676898557:web:9bda4e81a74703c4c66e94",
+      measurementId: "G-VB6Z9QN1G1"
+    };
+firebase.initializeApp(firebaseConfig);
+
+
+const messaging = firebase.messaging();
+
+ messaging.onBackgroundMessage((payload) => {
+      const notificationTitle = payload.notification.title;
+      const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+      };
+    
+      self.registration.showNotification(notificationTitle,notificationOptions);
     });
-    
-    
-    
-    self.addEventListener('activate', function(e) {
-        console.log('service worker is activated....')
-    });
-    
-    
