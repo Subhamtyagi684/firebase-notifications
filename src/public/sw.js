0c04992dd7b14f912083c840a761027e1678555f
Subhamtyagi684 <subhamtyagi85@gmail.com>
2022-03-23 18:53:34 +0530

    updated sw

--- src/public/sw.js
+++ src/public/sw.js
@@ -207,3 +207,10 @@ function broadcastReply(command, payload) {
   });
 }
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
